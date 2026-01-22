701100df3e9e4350875012b3f0f6bce745848abf
Petherson Erasmo <53876624+Petherson-Erasmo@users.noreply.github.com>
2026-01-22 23:32:48 +0000

    finsh `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test("GET /api/v1/status deveria retornar status 200 e { status: 'ok' }", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
